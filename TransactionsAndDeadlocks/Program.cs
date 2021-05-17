ae6fac776268dcbef18c6d5c78bb8c6737f030a4
explorer14 <amanag1983@gmail.com>
2021-05-17 21:31:52 +0200

    Add ado.net code

--- TransactionsAndDeadlocks/Program.cs
+++ TransactionsAndDeadlocks/Program.cs
@@ -1,12 +1,96 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Transactions;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
+using Org.BouncyCastle.Crypto.Tls;
 
 namespace TransactionsAndDeadlocks
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var repo = new Repository();
+            var id = 1000;
+
+            Parallel.For(0, 100,
+                async index =>
+                {
+                    var product = new Product
+                    {
+                        Id = id,
+                        Stock = index+1
+                    };
+                    
+                    using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+                    {
+                        await repo.Save(product);
+                        scope.Complete();
+                    }        
+                });
+            
+            Console.Read();
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public int Stock { get; set; }
+    }
+
+    public class Repository
+    {
+        public async Task Save(Product product)
+        {
+            using (var connection = new MySqlConnection("Server=127.0.0.1;Database=TrxDb;Uid=root;Password=pass123"))
+            {
+                await connection.OpenAsync();
+
+                if (await Exists(product, connection))
+                    await Update(product, connection);
+                else
+                    await Insert(product, connection);
+            }
+        }
+
+        private async Task Insert(Product product, MySqlConnection mySqlConnection)
+        {
+            var sql = "insert into TrxDb.Products values(@id, @stock)";
+            var cmd = mySqlConnection.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.Parameters.AddWithValue("@id", product.Id);
+            cmd.Parameters.AddWithValue("@stock", product.Stock);
+
+            var noOfRows = (long) await cmd.ExecuteNonQueryAsync();
+            Console.WriteLine($"Inserted {noOfRows} row(s)");
+        }
+
+        private async Task Update(Product product, MySqlConnection mySqlConnection)
+        {
+            var sql = "update TrxDb.Products set stock = @stock where Id = @id";
+            var cmd = mySqlConnection.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.Parameters.AddWithValue("@id", product.Id);
+            cmd.Parameters.AddWithValue("@stock", product.Stock);
+
+            var noOfRows = (long) await cmd.ExecuteNonQueryAsync();
+            Console.WriteLine($"Updated {noOfRows} row(s)");
+        }
+
+        private async Task<bool> Exists(Product product, MySqlConnection mySqlConnection)
+        {
+            var sql = "select count(1) from TrxDb.Products where Id = @id";
+            var cmd = mySqlConnection.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.Parameters.AddWithValue("@id", product.Id);
+
+            var noOfRows = (long) await cmd.ExecuteScalarAsync();
+
+            return noOfRows > 0;
         }
     }
 }
